2bf72adf0768d3deaf84f2a07beaa113d025084c
PavelSolovev <pashius2010@gmail.com>
2020-10-07 23:05:06 +0400

    initial commit

---
+++ src/components/Switcher.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+export default class Switcher extends Component {
+  state = {
+    color: "white",
+  };
+
+  setDart = () => {
+    this.setState({ color: "black" });
+  };
+
+  setLight = () => {
+    this.setState({ color: "white" });
+  };
+  
+  render() {
+    const { color } = this.state;
+    return <div style={{ padding: "10px", backgroundColor: color, height: "400px" }}></div>;
+  }
+}
