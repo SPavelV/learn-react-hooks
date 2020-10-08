29ed71239661700b58c3bf4f81a03c046da7c1e2
Solovev Pavel <p.solovev@elewise.com>
2020-10-08 19:39:20 +0400

    using useContext to get date from context

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import HookSwitcher from "./components/HookSwitcher";
 
 const MyContext = React.createContext();
@@ -12,13 +12,8 @@ function App() {
 }
 
 const Child = () => {
-  return (
-    <MyContext.Consumer>
-      {(value) => {
-        return <p> {value} </p>;
-      }}
-    </MyContext.Consumer>
-  );
+  const value = useContext(MyContext);
+  return <p>{value}</p>;
 };
 
 export default App;
